43b1aaca03139701b1f4d701d40a6a513bb8d1e3
Atul Jain <atul.jain2995@gmail.com>
2020-12-22 14:58:38 +0530

    Initial Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+const forecast = (longitude, latitude, callback) => {
+    url = 'http://api.weatherstack.com/current?access_key=f7b274eb5e6564852840a7fe9cf83a79&query='+latitude+','+longitude+'&units=m'
+    request({url,json:true},(error,{body}) => {
+        if(error){
+            callback('Unable to connect to internet',undefined)
+        }
+        else if(body.error)
+        {
+            callback('Request failed. Please try again with different location',undefined)
+        }
+        else{
+            const loc = body.current
+            callback(undefined,
+                'It\'s '+ loc.weather_descriptions+'here. Temperature is '+loc.temperature+' ,but it feels like '+
+                loc.feelslike)
+        }
+    })
+}
+
+module.exports = forecast
